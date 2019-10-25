e97554202a4c6956de0827deb7afd11a9dd23dc5
Alex Bianchi <alexbbianchi@gmail.com>
2019-10-25 08:26:00 -0300

    Alteraçõs do backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://alex:alex@cluster0-lk5kr.mongodb.net/tordilhoNegro2?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    // req.query = Acessa query params (para filtros);
-    // req.params = Acessar route params (para edição, delete);
-    // req.body = Acessar corpo da requisição (para criação, edição);
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+// req.query = Acessa query params (para filtros);
+// req.params = Acessar route params (para edição, delete);
+// req.body = Acessar corpo da requisição (para criação, edição);
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
